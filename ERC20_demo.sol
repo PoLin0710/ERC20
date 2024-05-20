2c84801aeb5fb55b53ceaafb5f34e0af0d4f31b2
PoLin <oliver590617@gmail.com>
2024-05-20 16:43:03 +0800

    Complete

---
+++ ERC20_demo.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract PandaCoin is ERC20 {
+
+    uint256 public maxSupply=10000000000000000000000; //10000 ether
+    address public owner;
+
+    modifier isOwner(address _from){
+        require(owner==_from,"your not owner");
+        _;
+    }
+
+    constructor(string memory _name,string memory _symbol) ERC20(_name,_symbol){
+        owner=msg.sender;
+    }
+
+    function mint(uint256 amount)external isOwner(msg.sender){
+        require(amount+totalSupply()<maxSupply);
+        _mint(msg.sender, amount);
+    } 
+}
